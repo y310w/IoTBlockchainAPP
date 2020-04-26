984208bb923e13c70ca92af180bef81c24bfbe36
Fernando Talavera <ftm1997@correo.ugr.es>
2020-04-26 23:43:35 +0200

    "WIP: script para la network"

--- infrastructure/config-network/network/networkiot.sh
+++ infrastructure/config-network/network/networkiot.sh
@@ -1,58 +1,101 @@
 #!/bin/bash
 
-printHelp() {
 
+printHelp() {
+    echo "Uso: networkiot.sh [help, up, down, generate, restart, remove]"
+    echo
+    echo "Comandos:"
+    echo "  help        Mostrar esta ayuda"
+    echo "  up          Activar red"
+    echo "  down        Desactivar red"
+    echo "  generate    Generar red"
+    echo "  restart     Reiniciar red"
+    echo "  remove      Eliminar red"
+    echo
 }
+
 
 generateNetwork() {
-    # remove previous crypto material and config transactions
-    rm -fr config/*
-    rm -fr crypto-config/*
-
-    # generate crypto material
-    cryptogen generate --config=./crypto-config.yaml
+    # eliminamos cualquier configuracion previa
+    rm -fr channel-artifacts/*
+    rm -fr crypto-config
+    
+    mkdir ./channel-artifacts/anchors
+    
+    # generamos crypto material
+    ../bin/cryptogen generate --config=./crypto-config.yaml
     if [ "$?" -ne 0 ]; then
-    echo "Failed to generate crypto material..."
+    echo "Fallo al generar crypto material..."
     exit 1
     fi
 
-    # generate genesis block for orderer
-    configtxgen -profile OneOrgOrdererGenesis -outputBlock ./config/genesis.block
+    # generamos bloque genesis para el orderer
+    ../bin/configtxgen -profile OrdererGenesis -outputBlock ./channel-artifacts/genesis.block
     if [ "$?" -ne 0 ]; then
-    echo "Failed to generate orderer genesis block..."
+    echo "Fallo al generar el orderer genesis block..."
     exit 1
     fi
 
-    # generate channel configuration transaction
-    configtxgen -profile OneOrgChannel -outputCreateChannelTx ./config/channel.tx -channelID $CHANNEL_NAME
+    # generamos la configuracion del canal Device
+    ../bin/configtxgen -profile DeviceChannel -outputCreateChannelTx ./channel-artifacts/deviceChannel.tx -channelID devicechannel
     if [ "$?" -ne 0 ]; then
-    echo "Failed to generate channel configuration transaction..."
+    echo "Fallo al generar la configuración del canal Device..."
     exit 1
     fi
 
-    # generate anchor peer transaction
-    configtxgen -profile OneOrgChannel -outputAnchorPeersUpdate ./config/Org1MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org1MSP
+    # generamos el anchor peer transaction
+    ../bin/configtxgen -profile DeviceChannel -outputAnchorPeersUpdate ./channel-artifacts/anchors/DeviceMSPanchors.tx -channelID dhanchor -asOrg DeviceMSP
     if [ "$?" -ne 0 ]; then
-    echo "Failed to generate anchor peer update for Org1MSP..."
+    echo "Fallo al generar el anchor peer para DeviceMSP..."
+    exit 1
+    fi
+
+    # generamos el anchor peer transaction
+    ../bin/configtxgen -profile DeviceChannel -outputAnchorPeersUpdate ./channel-artifacts/anchors/DHandlerMSPanchors.tx -channelID hdanchor -asOrg HandlerMSP
+    if [ "$?" -ne 0 ]; then
+    echo "Fallo al generar el anchor peer de Device para HandlerMSP..."
+    exit 1
+    fi
+
+    # generamos la configuracion del canal Region
+    ../bin/configtxgen -profile RegionChannel -outputCreateChannelTx ./channel-artifacts/regionChannel.tx -channelID regionchannel
+    if [ "$?" -ne 0 ]; then
+    echo "Fallo al generar la configuración del canal Region..."
+    exit 1
+    fi
+
+    # generamos el anchor peer transaction
+    ../bin/configtxgen -profile RegionChannel -outputAnchorPeersUpdate ./channel-artifacts/anchors/RegionMSPanchors.tx -channelID rhanchor -asOrg RegionMSP
+    if [ "$?" -ne 0 ]; then
+    echo "Fallo al generar el anchor peer para RegionMSP..."
+    exit 1
+    fi
+
+    # generamos el anchor peer transaction
+    ../bin/configtxgen -profile RegionChannel -outputAnchorPeersUpdate ./channel-artifacts/anchors/RHandlerMSPanchors.tx -channelID hranchor -asOrg HandlerMSP
+    if [ "$?" -ne 0 ]; then
+    echo "Fallo al generar el anchor peer de Region para HandlerMSP..."
     exit 1
     fi
 }
+
 
 startNetwork() {
     set -ev
 
-    # don't rewrite paths for Windows Git Bash users
-    export MSYS_NO_PATHCONV=1
+    docker-compose -f docker-compose.yaml down
 
-    docker-compose -f docker-compose.yml down
-
-    docker-compose -f docker-compose.yml up -d ca.example.com orderer.example.com peer0.org1.example.com couchdb
+    rsync -r channel-artifacts/ ubuntu@192.168.0.32:~/channel-artifacts
+    rsync -r crypto-config/ ubuntu@192.168.0.32:~/crypto-config
+    rsync -r ../chaincode/ ubuntu@192.168.0.32:~/../chaincode
+    
+    docker stack deploy --compose-file docker-compose.yaml blockchain && docker ps
+    docker-compose -f docker-compose.yaml up -d
     docker ps -a
 
-    # wait for Hyperledger Fabric to start
-    # incase of errors when running later commands, issue export FABRIC_START_TIMEOUT=<larger number>
+    
+    # Esperarmos a que este todo montado
     export FABRIC_START_TIMEOUT=10
-    #echo ${FABRIC_START_TIMEOUT}
     sleep ${FABRIC_START_TIMEOUT}
 
     # Create the channel
@@ -61,35 +104,45 @@ startNetwork() {
     docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer0.org1.example.com peer channel join -b mychannel.block
 }
 
+
 stopNetwork() {
     set -ev
 
-    # Shut down the Docker containers that might be currently running.
-    docker-compose -f docker-compose.yml stop
+    docker stack rm blockchain
+    # Apagamos los docker images que esten corriendo
+    docker-compose -f docker-compose.yaml stop
 }
 
-teardownNetwork() {
+
+removeNetwork() {
     set -e
 
-    # Shut down the Docker containers for the system tests.
-    docker-compose -f docker-compose.yml kill && docker-compose -f docker-compose.yml down
+    # Apagamos los docker images que esten corriendo
+    docker-compose -f docker-compose.yaml kill && docker-compose -f docker-compose.yaml down
 
-    # remove the local state
+    # eliminamos el estado local
     rm -f ~/.hfc-key-store/*
 
-    # remove chaincode docker images
+    # eliminamos los docker de chaincode
     docker rm $(docker ps -aq)
     docker rmi $(docker images dev-* -q)
 }
 
-if [ "${MODE}" == "up" ]; then
+
+MODE=$1
+
+if [ "${MODE}" == "up" ]; then                              # Activar red
     startNetwork
-elif [ "${MODE}" == "down" ]; then ## Clear the network
-    networkDown
-elif [ "${MODE}" == "generate" ]; then ## Generate Artifacts
-    generateNetwork
-elif [ "${MODE}" == "restart" ]; then ## Restart the network
+elif [ "${MODE}" == "down" ]; then                          # Desactivar red
     stopNetwork
+elif [ "${MODE}" == "generate" ]; then                      # Generar red
+    generateNetwork
+elif [ "${MODE}" == "restart" ]; then                       # Reiniciar red
     startNetwork
-elif [ "${MODE}" == "teardown" ]; then ## Restart the network
-    teardownNetwork
+    stopNetwork
+elif [ "${MODE}" == "remove" ]; then                        # Eliminar red
+    removeNetwork
+elif [ "${MODE}" == "" ] || [ "${MODE}" == "help" ]; then   # Mostrar la ayuda 
+    printHelp
+    exit 0
+fi
