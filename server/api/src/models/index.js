ebc140cfea1936e3d8d78d059adfab6af87f0518
Fernando Talavera <ftm1997@correo.ugr.es>
2020-09-15 13:39:08 +0200

    Mongoose, autorizaciones y usuario eliminados

--- server/api/src/models/index.js
+++ server/api/src/models/index.js
@@ -1,22 +1,6 @@
-import mongoose from 'mongoose';
-
-import User from './user';
 import Device from './device';
 import Linkage from './linkage';
 
-const connectDb = () => {
-    return mongoose.connect(
-        process.env.TEST_DB_URI || process.env.MONGODB_URI,
-        {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useCreateIndex: true
-        }
-    );
-};
-
-const models = { User, Device, Linkage };
-
-export { connectDb };
+const models = { Device, Linkage };
 
 export default models;
