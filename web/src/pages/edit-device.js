8c1c3bd96444fcb2dcd342b9dadbf0c0b1e478a9
Fernando Talavera <ftm1997@correo.ugr.es>
2020-10-06 22:24:34 +0200

    Comprobar device antes de rendericar

--- web/src/pages/edit-device.js
+++ web/src/pages/edit-device.js
@@ -27,49 +27,64 @@ const HISTORY_DEVICE = gql`
 export default function EditDevice({ location }) {
   const device = location.state ? location.state.data : undefined;
   
-  const { loading, error, data } = useQuery(HISTORY_DEVICE, {
-    variables: { serial: device.serial },
-    fetchPolicy: "cache-and-network"
-  });
-
-  let output = (
-    <div>
-      { !loading && <TableData data={data.historyDevice} editable="false"/> }
-    </div>
-  );
-
-  if (loading) output = <div><p>Loading...</p></div>
-  if (error) output = <div><p>Error, no se ha podido recopilar el historial. {error}</p></div>
-
-  const [deleteDevice] = useMutation(REMOVE_DEVICE, {onCompleted: successMessage});
-
-  function removeDevice() {
-    deleteDevice({ 
-      variables: {  
-        serial: device.serial, 
-      } 
+  if (device) {
+    const { loading, error, data } = useQuery(HISTORY_DEVICE, {
+      variables: { serial: device.serial },
+      fetchPolicy: "cache-and-network"
     });
-  }
 
-  function successMessage() {
-    navigate('/devices/', {state: { success: true, message: 'Se ha borrado con exito', policy: "cache-and-network"}})
-  }
-
-  return (
-    <Layout> 
-      <div className="outer">
-        <div className="inner">
-          <CardTitle className="is-light-text text-x-large letter-spacing"> Editar dispositivo </CardTitle>
-        </div>
-        <div className="inner add-button">
-          <Button color="danger" onClick={removeDevice}> - Eliminar dispositivo </Button>
-        </div>
-      </div>
-      <FormDevice data={device} />
+    let output = (
       <div>
-        <CardTitle className="is-light-text text-x-large letter-spacing"> Historial </CardTitle>
-        {output}
+        { !loading && <TableData data={data.historyDevice} editable="false"/> }
       </div>
-    </Layout>
-  )
+    );
+
+    if (loading) output = <div><p>Loading...</p></div>
+    if (error) output = <div><p>Error, no se ha podido recopilar el historial. {error}</p></div>
+
+    const [deleteDevice] = useMutation(REMOVE_DEVICE, {onCompleted: successMessage});
+
+    function removeDevice() {
+      deleteDevice({ 
+        variables: {  
+          serial: device.serial, 
+        } 
+      });
+    }
+
+    function successMessage() {
+      navigate('/devices/', {state: { success: true, message: 'Se ha borrado con exito', policy: "cache-and-network"}})
+    }
+
+    return (
+      <Layout> 
+        <div className="outer">
+          <div className="inner">
+            <CardTitle className="is-light-text text-x-large letter-spacing"> Editar dispositivo </CardTitle>
+          </div>
+          <div className="inner add-button">
+            <Button color="danger" onClick={removeDevice}> - Eliminar dispositivo </Button>
+          </div>
+        </div>
+        <FormDevice data={device} />
+        <div>
+          <CardTitle className="is-light-text text-x-large letter-spacing"> Historial </CardTitle>
+          {output}
+        </div>
+      </Layout>
+    )
+  } else {
+    return (
+      <Layout> 
+        <div className="outer">
+          <div className="inner">
+            <CardTitle className="is-light-text text-x-large letter-spacing"> Editar dispositivo </CardTitle>
+          </div>
+        </div>
+        <div>
+          <CardTitle className="is-light-text text-x-large letter-spacing"> Historial </CardTitle>
+        </div>
+      </Layout>
+    )
+  }
 }
